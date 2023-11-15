39fe6a71a73b8e238fd5a3a4e3ac57128b05b131
Harshi <dsharshithasonu@gmail.com>
2023-11-15 11:28:23 +0530

    Demo Class pushed

---
+++ src/test/java/Practice/Practice_01.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class Practice_01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Github");
+		System.out.println("Haii Github");
+
+	}
+
+}
